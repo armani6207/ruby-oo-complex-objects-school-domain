99530fee685135f12c828b3bab345b897afdff0a
Armando <rojas.armani@gmail.com>
2020-11-18 20:01:08 -0500

    commit tree 095a5db9c233864c193bb2dfad5249b05ccf88e1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+       
+       if @roster.keys.include?(grade)
+        roster[grade] << name
+       else
+        @roster[grade] = []
+        @roster[grade] << name
+       end
+    end
+
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort
+        @roster.map {|key, value| @roster[key] = value.sort}
+        @roster
+    end
+
+end
